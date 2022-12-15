583917d2502a52460f3d2d446fd56176e1dd6ef6
Qi Mingtong <qimingtong518@gmail.com>
2022-12-15 17:28:04 +0800

    static pages resources

--- src/test/java/org/example/note/TestDB.java
+++ src/test/java/org/example/note/TestDB.java
@@ -2,12 +2,19 @@ package org.example.note;
 
 import org.example.note.util.DBUtil;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.sql.SQLException;
 
 public class TestDB {
+
+    private Logger logger = LoggerFactory.getLogger(TestDB.class);
+
     @Test
     public void testDB() throws SQLException {
         System.out.println(DBUtil.getConnection());
+
+        logger.info("获取数据库连接：" + DBUtil.getConnection());
     }
 }
