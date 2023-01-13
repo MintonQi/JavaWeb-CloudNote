d05d6711ac730490d5d640e34b33cbccec0160f6
Qi Mingtong <qimingtong518@gmail.com>
2023-01-14 00:38:26 +0800

    BaseDao added

--- src/test/java/org/example/note/TestUser.java
+++ src/test/java/org/example/note/TestUser.java
@@ -1,10 +1,13 @@
 package org.example.note;
 
+import org.example.note.dao.BaseDao;
 import org.example.note.dao.UserDao;
 import org.example.note.po.User;
 import org.junit.Test;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class TestUser {
@@ -16,5 +19,17 @@ public class TestUser {
         System.out.println(user.getUpwd());
     }
 
+    @Test
+    public void testAdd() throws SQLException {
+        String sql = "insert into tb_user(uname,upwd,nick,head,mood) values(?,?,?,?,?)";
+        List<Object> params = new ArrayList<>();
+        params.add("lisi");
+        params.add("e10adc3949ba59abbe56e057f20f883e");
+        params.add("lisi");
+        params.add("404.jpg");
+        params.add("Hello");
+        int row = BaseDao.executeUpdate(sql,params);
+        System.out.println(row);
+    }
 
 }
